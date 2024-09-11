ff8bad846f106f44b877ae37befe459a7123cca8
DavidGame12 <david.flores@iteesa.edu.hn>
2024-09-11 08:47:12 -0600

    tabien

---
+++ resources/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Hola desde app de vue jehrjae
+        <a href="/login">Login</a>
+    </div>
+</template>
+<script>
+export default {
+    data:() =>({
+
+   }),
+
+   
+}</script>
